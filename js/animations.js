a57d2f257d0df843ffa44736b7a0cfb6c34f6bbd
timothykool <tkoo005@ucr.edu>
2019-02-10 00:46:10 -0800

    Added sections and styling

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,7 @@
 var typed = new Typed('#typed', {
 	stringsElement: '#typed-strings',
 	startDelay: 10,
-	typeSpeed: 100,
+	typeSpeed: 50,
 	backSpeed: 70,
 	showCursor: true,
   	cursorChar: '|',
@@ -9,8 +9,14 @@ var typed = new Typed('#typed', {
 	smartBackspace: true,
 });
 
-var typed2 = new Typed('#typed2', {
-	strings: [
-	  "git push --force ^1000\n `pushed to origin with option force`"
-	]
-});
+// alert(screen.width);
+
+if (screen.width < 1166) {
+	document.getElementById("column2").innerHTML = "Activities";
+}
+
+// var typed2 = new Typed('#typed2', {
+// 	strings: [
+// 	  "git push --force ^1000\n `pushed to origin with option force`"
+// 	]
+// });
