29177a9e2191a014bb95f3b2d6128c0dfb5c0e7d
timothykool <tkoo005@ucr.edu>
2019-03-05 21:56:18 -0800

    Navbar redone

--- js/animations.js
+++ js/animations.js
@@ -11,12 +11,23 @@ var typed = new Typed('#typed', {
 
 // alert(screen.width);
 
-if (screen.width < 1166) {
-	document.getElementById("column2").innerHTML = "Activities";
-}
-
 // var typed2 = new Typed('#typed2', {
 // 	strings: [
 // 	  "git push --force ^1000\n `pushed to origin with option force`"
 // 	]
 // });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
